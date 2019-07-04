96f6cc8350acfda1c62ded1f5ed0ecdd37d72830
root <root@rhos-nfv-08.lab.eng.rdu2.redhat.com>
2019-07-04 01:30:57 -0400

    Deploy OSP13 in NFV lab (node 8 and 9).

--- osp13_ref/overcloud_deploy.sh
+++ osp13_ref/overcloud_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! -f 'roles_data.yaml' ]]; then
+if [[ ! -f "$HOME/roles_data.yaml" ]]; then
   openstack overcloud roles generate -o ~/roles_data.yaml Controller ComputeOvsDpdkSriov
 fi
 
@@ -13,6 +13,6 @@ openstack overcloud deploy --templates \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+    -e ~/osp13_ref/host-config-and-reboot.yaml \
     -e ~/osp13_ref/environment.yaml \
-    -e ~/osp13_ref/docker-images.yaml
+    -e ~/osp13_ref/overcloud_images.yaml
